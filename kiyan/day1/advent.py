be7b03e00f42447b9939ccc4e983892d86c6c45e
Kiyan Zewer <kiyanzewer@gmail.com>
2024-12-04 17:06:33 -0500

    part 2

--- kiyan/day1/advent.py
+++ kiyan/day1/advent.py
@@ -1,3 +1,5 @@
+# Part 1
+
 # Read
 with open('inputs.txt', 'r') as file:
     one, two = zip(*((int(x), int(y)) for x, y in (line.strip().split() for line in file)))
@@ -10,3 +12,23 @@ two = sorted(two)
 sum = sum(abs(i - j) for i, j in zip(one, two))
 
 print(sum)
+
+# Part 2
+# Go through one first
+occurrences_one = {}
+
+# Count occurrences in 'one'
+for i in one:
+    if i in occurrences_one:
+        occurrences_one[i] += 1
+    else:
+        occurrences_one[i] = 1
+
+# Calculate similarity
+similarity = 0
+for i in two:
+    if i in occurrences_one:
+        similarity += i * occurrences_one[i]
+
+print(similarity)
+
